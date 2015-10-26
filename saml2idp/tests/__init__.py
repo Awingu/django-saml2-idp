1ffe277cfd04d73ee7b0b0e5ed893715ca81d949
Mohab Usama <mohab.usama@gmail.com>
2015-10-26 11:59:29 +0200

    Removed imports in tests __init__

--- saml2idp/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-from deeplink import *  # noqa
-from google_apps import *  # noqa
-from salesforce import *  # noqa
-from azure import *  # noqa
-from signing import *  # noqa
-from views import *  # noqa
